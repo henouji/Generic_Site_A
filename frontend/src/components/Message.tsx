1adae4659c56e7ec69ae7568078e7facb4c69583
ft-light <cfeposerio@gmail.com>
2023-07-04 13:57:04 +0800

    chore: initialize folder structure

---
+++ frontend/src/components/Message.tsx
@@ -0,0 +1,9 @@
+function Message() {
+	return (
+		<h1 className="text-3xl font-bold underline">
+			Hello World!
+		</h1>
+	);
+}
+
+export default Message;
---
+++ frontend/src/pages/App.tsx
@@ -0,0 +1,8 @@
+import Message from '../components/Message'
+import '../assets/App.css'
+
+function App() {
+  return <><Message /></>
+}
+
+export default App;
