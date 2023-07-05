f2a1c6bccb6c5ed842eec4a5d15d62d7cc5d7ace
ft-light <cfeposerio@gmail.com>
2023-07-05 13:36:18 +0800

    feat: create a generic login form

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,39 @@
+import { 
+  Box,
+  Button,
+  Center,
+  Container,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Image,
+  Input
+} from "@chakra-ui/react";
+
+function Login() {
+  return (
+    <Flex width="100vw" height="90vh" alignItems="center" justifyContent="center">
+      <Container maxW="sm">
+        <Center>
+          <Image boxSize='50px' src='/react.svg' alt='React App' />
+        </Center>
+        <Heading textAlign="center" size="md" my="30px">Generic Site A</Heading>
+        <FormControl>
+          <FormLabel>Username</FormLabel>
+          <Input type='text' focusBorderColor='purple' size="sm"/>
+        </FormControl>
+        <FormControl my="20px">
+          <FormLabel>Password</FormLabel>
+          <Input type='password' focusBorderColor='purple' size="sm"/>
+        </FormControl>
+        <Box>
+          <Button type="submit" colorScheme="purple" width="100%" size="sm">Submit</Button>
+        </Box>
+      </Container>
+    </Flex>
+  );
+
+}
+
+export default Login;
