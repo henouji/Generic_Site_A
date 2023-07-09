b8a456e2a1b76c4c86e133a9c8ecba12a663b435
ft-light <cfeposerio@gmail.com>
2023-07-09 16:41:26 +0800

    feat: add navigation function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,17 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import DashboardLayout from './layouts/DashboardLayout';
+import Jobs from './pages/Jobs';
+import Filler from './pages/Filler';
 
 const router = createBrowserRouter([
   {
     element: <DashboardLayout />,
     children: [
       { path: "dashboard", element: <Dashboard /> },
+      { path: "jobs", element: <Jobs /> },
+      { path: "lorem", element: <Filler /> },
+      { path: "ipsum", element: <Filler /> },
     ]
   },
   { path: "/", element: <Home /> },
